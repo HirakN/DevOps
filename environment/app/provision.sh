d551afdb4bc359cff3739db34c7d2a0af4c1eefb
Tech-A43 <tech-a43@Tech-A43s-MacBook-Air.local>
2018-01-29 19:50:50 +0000

    added VM initilisation scripts for bash with installation for node v6 and a npm package

--- environment/app/provision.sh
+++ environment/app/provision.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+# update and upgrade
+sudo apt-get update -y
+sudo apt-get upgrade -y 
+
+# Install 
+sudo apt-get install nginx -y
+
+# Install nodejs v6
+sudo apt-get install nodejs -y
+sudo curl -sL https://deb.nodesource.com/setup_6.x -o nodesource_setup.sh
+sudo bash ./nodesource_setup.sh
+sudo apt-get install nodejs -y
+
+# Install pm2 wiht npm
+sudo npm install pm2 -g
